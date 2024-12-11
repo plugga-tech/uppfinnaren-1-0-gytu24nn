1891b4e4a210e9f776933a7d2f7c96f140661af0
Tuva Gyllensten <tuvagyllensten@gmail.com>
2024-12-11 09:39:09 +0100

    lagt till mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace uppfinnaren_1_0_gytu24nn.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
