4fd74dfa6f6acfac4ddf28d5028ae4b1790302dd
Tuva Gyllensten <tuvagyllensten@gmail.com>
2024-12-12 10:08:13 +0100

    skrivit data för konstverk

--- Controllers/ArtworkController.cs
+++ Controllers/ArtworkController.cs
@@ -1,13 +1,22 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using uppfinnaren_1_0_gytu24nn.Models;
 
 namespace uppfinnaren_1_0_gytu24nn.Controllers;
 
 public class ArtworkController : Controller
 {
+
+    private readonly IArtworkRepository _artworkRepository;
+
+    public ArtworkController(IArtworkRepository artworkRepository)
+    {
+        _artworkRepository = artworkRepository;
+    }
+
     public IActionResult Index()
     {   
         ViewBag.Title = "Konstverk";
-        return View();
+        return View(_artworkRepository.AllArtwork);
     }   
 }
