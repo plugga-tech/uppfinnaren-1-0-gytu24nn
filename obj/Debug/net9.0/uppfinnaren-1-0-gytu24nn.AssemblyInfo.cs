789f93463b8a959d13eafc244b112a8aa626765b
Tuva Gyllensten <tuvagyllensten@gmail.com>
2024-12-12 21:26:28 +0100

    lagt till partial view

--- obj/Debug/net9.0/uppfinnaren-1-0-gytu24nn.AssemblyInfo.cs
+++ obj/Debug/net9.0/uppfinnaren-1-0-gytu24nn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uppfinnaren-1-0-gytu24nn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f66bf993f728185e6574a8ac2fb8e2c25bf196d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93854354d5a03df9f199410499ad4c424e3497c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("uppfinnaren-1-0-gytu24nn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uppfinnaren-1-0-gytu24nn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
